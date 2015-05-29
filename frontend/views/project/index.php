30418af8515d861b6a699abe4118c01f4ffb951c
Marvin <samon127@gmail.com>
2015-05-29 19:43:20 +0800

    upload
    upload

--- frontend/views/project/index.php
+++ frontend/views/project/index.php
@@ -1,13 +1,34 @@
-<table data-toggle="table"
-       data-url="/gh/get/response.json/wenzhixin/bootstrap-table/tree/master/docs/data/data1/"
-       data-classes="table table-hover table-condensed"
-       data-striped="true">
+<?php 
+use yii\web\View;
+?>
+<!-- DataTables CSS -->
+
+<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.7/css/jquery.dataTables.css">
+
+<!-- DataTables -->
+<?php $this->registerJsFile('http://cdn.datatables.net/1.10.7/js/jquery.dataTables.js', ['depends' => [\yii\web\JqueryAsset::className()], 'position' => View::POS_HEAD]); ?>
+
+<table id="table_id" class="display">
     <thead>
-    <tr>
-        <th class="col-xs-4" data-field="name">Name</th>
-        <th class="col-xs-1" data-field="stargazers_count">Stars</th>
-        <th class="col-xs-1" data-field="forks_count">Forks</th>
-        <th class="col-xs-6" data-field="description">Description</th>
-    </tr>
+        <tr>
+            <th>Column 1</th>
+            <th>Column 2</th>
+        </tr>
     </thead>
+    <tbody>
+        <tr>
+            <td>Row 1 Data 1</td>
+            <td>Row 1 Data 2</td>
+        </tr>
+        <tr>
+            <td>Row 2 Data 1</td>
+            <td>Row 2 Data 2</td>
+        </tr>
+    </tbody>
 </table>
+
+<script>
+$(document).ready( function () {
+    $('#table_id').DataTable();
+} );
+</script>
